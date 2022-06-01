9e69627abf522e81d505ac2253579efb92d3b5ba
wanjikujanee <wanjikumaryjanee@gmail.com>
2022-06-01 20:45:37 +0300

    first commit

--- app/src/main/java/dev/jane/workoutlog/LoginActivity.kt
+++ app/src/main/java/dev/jane/workoutlog/LoginActivity.kt
@@ -6,15 +6,56 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var tvSignUp:TextView
+    lateinit var tilEmail:TextInputLayout
+    lateinit var tilPassword: TextInputLayout
+    lateinit var btnLogin:Button
+    lateinit var etTextView:TextView
+
 
   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+      tvSignUp=findViewById(R.id.tvSignUp)
+      tilEmail=findViewById(R.id.tilEmail)
+      etTextView=findViewById(R.id.etTextView)
+      tilPassword=findViewById(R.id.tilPassword)
+      btnLogin=findViewById(R.id.btnLogin)
 
+
+
+
+
+      tvSignUp.setOnClickListener {
+          val intent = Intent(this, SignUpActivity::class.java)
+          startActivity(intent)
+
+      }
+      btnLogin.setOnClickListener{
+          validateLogin()
+      }
+  }
+
+    fun validateLogin() {
+        var email=tilEmail.editText.toString()
+        var password=tilPassword.editText.toString()
+        if(email.isBlank()){
+            tilEmail.error="Email is invalid"
         }
+        if (password.isBlank()){
+            tilPassword.error="Password is required"
+        }
+    }
 }
+
+
+
+
+
+
 
 
 
--- app/src/main/java/dev/jane/workoutlog/SignUpActivity.kt
+++ app/src/main/java/dev/jane/workoutlog/SignUpActivity.kt
@@ -1,11 +1,74 @@
 package dev.jane.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var tvLogin: TextView
+    lateinit var btnSignUp: Button
+    lateinit var tilEmail: TextInputLayout
+    lateinit var tilfirstName: TextInputLayout
+    lateinit var tilPassword: TextInputLayout
+    lateinit var tillastName: TextInputLayout
+    lateinit var tilconfirm: TextInputLayout
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        tvLogin=findViewById(R.id.tvSignUp)
+        btnSignUp=findViewById(R.id.btnSignUp)
+        tillastName=findViewById(R.id.tillastName)
+        tilconfirm=findViewById(R.id.tilconfirm)
+        tilEmail=findViewById(R.id.tilEmail)
+        tilfirstName=findViewById(R.id.tilfirstName)
+        tilPassword=findViewById(R.id.tilPassword)
+
+
+        tvLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        btnSignUp.setOnClickListener{
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+           invalidateSignUp()
+        }
     }
+
+    fun invalidateSignUp() {
+        var firstname=tilfirstName.editText.toString()
+        var lastname=tillastName.editText.toString()
+        var email=tilEmail.editText.toString()
+        var password=tilPassword.editText.toString()
+        var confirm=tilconfirm.editText.toString()
+
+        if(firstname.isBlank()){
+            tilfirstName.error="Email is invalid"
+        }
+        if (lastname.isBlank()){
+            tillastName.error="Password is required"
+        }
+
+        if(email.isBlank()){
+            tilEmail.error="Email is invalid"
+        }
+        if (password.isBlank()){
+            tilPassword.error="Password is required"
+        }
+        if (confirm.isBlank()){
+            tilconfirm.error="Password is required"
+        }
+
+
+    }
+
+
 }
