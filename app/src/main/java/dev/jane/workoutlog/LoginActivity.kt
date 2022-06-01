4e7a4526c1cb71ebc29fc2f9138da31febf4bbec
wanjikujanee <wanjikumaryjanee@gmail.com>
2022-06-01 15:42:41 +0300

    first commit

---
+++ app/src/main/java/dev/jane/workoutlog/LoginActivity.kt
@@ -0,0 +1,20 @@
+package dev.jane.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+
+class LoginActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        }
+}
+
+
+
